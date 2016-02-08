317a63aceb075cc416685dd143359984f4c593c7
Kevin Johnson <bluechaos@gmail.com>
2016-02-08 09:23:39 -0700

    Made it so the floor could be black - it was a MeshBasicMaterial so no shading was being applied to it. By changing it to a MeshLambertMaterial it works as expected. I threw in a faint ambient light so I could test how well it works, and it does great. I disabled the fog since it shouldn't be necessary, but we can add it in if desired.

--- enviroment.js
+++ enviroment.js
@@ -111,8 +111,8 @@ function init()
 	//var light = new THREE.PointLight(0xffffff);
 	//light.position.set(0,250,0);
 	//scene.add(light);
-	//var ambientLight = new THREE.AmbientLight(0x111111);
-	// scene.add(ambientLight);
+	var ambientLight = new THREE.AmbientLight(0x444444);
+    scene.add(ambientLight);
 
 	//////////////
 	// GEOMETRY //
@@ -137,7 +137,7 @@ function init()
 	floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
 	floorTexture.repeat.set( 1, 1 );
 	// DoubleSide: render texture on both sides of mesh
-	var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+	var floorMaterial = new THREE.MeshLambertMaterial( { map: floorTexture, side: THREE.DoubleSide } );
 	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 1, 1);
 	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
 	floor.position.y = -0.5;
@@ -156,10 +156,10 @@ function init()
 	// BackSide: render faces from inside of the cube, instead of from outside (default).
 	var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, side: THREE.BackSide } );
 	var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-	// scene.add(skyBox);
+	scene.add(skyBox);
 
 	// fog must be added to scene before first render
-	scene.fog = new THREE.FogExp2( 0x000000, 0.003500 );
+	//scene.fog = new THREE.FogExp2( 0x000000, 0.003500 );
 }
 
 function animate()
